f57d07369d55ce9e43eae20899f2e70469f19457
lbseven7 <leobarbosapinturas@gmail.com>
2022-12-13 16:29:52 -0300

    feat: function filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -4,8 +4,8 @@ import csv
 
 
 @lru_cache
-def read(path: str, encoding="utf-8") -> List[Dict]:
-    with open(path, "r") as read_job:
+def read(path: str) -> List[Dict]:
+    with open(path, "r", encoding="utf-8") as read_job:
         data = csv.DictReader(read_job)
         data_list = []
         for row in data:
@@ -22,8 +22,8 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    read_jobs = read(jobs)
-    filter_job_types = set()
-    for type in read_jobs:
-        filter_job_types.add(type["job_type"])
+    filter_job_types = list()
+    for type in jobs:
+        if (type["job_type"] == job_type):
+            filter_job_types.append(type)
     return filter_job_types
