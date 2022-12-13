a056c971e3a31a4c785317f10bc9a45e9b82af91
lbseven7 <leobarbosapinturas@gmail.com>
2022-12-13 15:44:32 -0300

    feat: function filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -22,18 +22,8 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    read_jobs = read(jobs)
+    filter_job_types = set()
+    for type in read_jobs:
+        filter_job_types.add(type["job_type"])
+    return filter_job_types
