6a401f079ec5af631636144aed9e8a49c2d83115
lbseven7 <leobarbosapinturas@gmail.com>
2022-12-14 13:08:31 -0300

    feat: function filter_by_industry

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 from src.insights.jobs import read
 
 
+# 3 - Implemente a função get_unique_industries
 def get_unique_industries(path: str) -> List[str]:
     read_jobs = read(path)
     filter_industry = set()
@@ -11,19 +12,10 @@ def get_unique_industries(path: str) -> List[str]:
     return filter_industry
 
 
+# 7 - Implemente a função filter_by_industry
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    filter_industry = list()
+    for job_industry in jobs:
+        if (job_industry["industry"] == industry):
+            filter_industry.append(job_industry)
+    return filter_industry
