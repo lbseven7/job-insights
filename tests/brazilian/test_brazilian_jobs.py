661161a83cec21e0d19598931f0bebb51811e01c
lbseven7 <leobarbosapinturas@gmail.com>
2022-12-19 17:19:34 -0300

    feat: function test_brazilian_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -3,12 +3,14 @@ from src.pre_built.brazilian_jobs import read_brazilian_file
 
 # 11 - Implemente um teste para a função read_brazilian_file
 def test_brazilian_jobs():
-    brazilian_job = read_brazilian_file(
-        'tests/mocks/brazilians_jobs.csv')
-    for job in brazilian_job:
-        job["titulo"] = job.pop("title")
-        job["salario"] = job.pop("salary")
-        job["tipo"] = job.pop("type")
-    return brazilian_job
-
-# [{'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'}]
+    csv = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert 'title' in csv[1] and 'salary' in csv[1] and 'type' in csv[1]
+    assert 'titulo' not in csv[1]
+    assert 'salario' not in csv[1]
+    assert 'tipo' not in csv[1]
+    # O dicionário:
+    # {"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}
+    # Deve ser traduzido para:
+    # {"title": "Maquinista", "salary": "2000", "type": "trainee"}
+    print('AQUI RESULTADO TRADUZIDO ===>', csv[0])
+    # {'title': 'Maquinista', 'salary': '2000', 'type': 'trainee'}
